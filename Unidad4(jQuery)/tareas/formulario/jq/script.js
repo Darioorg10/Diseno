44321fcf01604a5292361c2e01c5bd812025b5fd
Dario <dricgar1103@g.educaand.es>
2024-03-05 13:54:04 +0100

    Ejercicio formulario hecho

---
+++ Unidad4(jQuery)/tareas/formulario/jq/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+
+    // Cuando los campos requeridos pierdan el foco se debe comprobar
+    // si la longitud del campo es 0, si lo es, debajo del campo aparece
+    // un mensaje en rojo indicando que no puede estar vacío, cuando
+    // haya un nuevo error no se pueden borrar los anteriores, y cuando
+    // la condición de error no se cumpla, se borra
+    $("form input").on({
+        focusout:function(){
+            if ($(this).prop("required") == true) { // Cuando el input sea required
+                
+                if ($(this).prop("type") == "email") {
+                    if ($(this).val().length == 0) {
+                        $(this).siblings("#error-email").html("Campo obligatorio*").css("color", "red")
+                    } else {
+                        $(this).siblings("#error-email").html("")
+                    }
+                } else {
+                    if ($(this).val().length == 0) {
+                        $(this).siblings("#error-clave").html("Campo obligatorio*").css("color", "red")
+                    } else {
+                        $(this).siblings("#error-clave").html("")
+                    }
+                }            
+
+            }
+        }
+    })
+
+
+    // Cada vez que se pulse una tecla en el textarea ponemos el número de carácteres restantes
+    $("form textarea").on({
+        keydown:function(){
+            var a = $(this).val().length
+            if (a < 100) {
+                var restantes = 100 - a - 1// Calculamos los carácteres
+                if (restantes > 0) {
+                    $("#info-t-area").html("Dispone de " + restantes + " carácteres").css("color", "green")
+                } else {
+                    $("#info-t-area").html("Dispone de " + restantes + " carácteres").css("color", "red")
+                }
+            }                        
+        }
+    })
+
+    // Usamos el plugin de datepicker
+    $("#datepicker").datepicker();
+
+});
